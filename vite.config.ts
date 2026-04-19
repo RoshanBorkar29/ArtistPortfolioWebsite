02a40a2923a9f3e0ad8a2513a373f6cd9ae451b7
RoshanBorkar29 <roshan.22310181@viit.ac.in>
2026-04-19 20:02:47 +0530

    Updated the root files!!

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // This ensures paths map correctly on GitHub Pages where the site is served under /ArtistPortfolioWebsite/
+  base: '/ArtistPortfolioWebsite/'
+});
